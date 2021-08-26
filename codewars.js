964f0ec067240130772a6e446ee1cf5d5db46805
Benjamin Fultz <xsymbiotex@gmail.com>
2021-08-26 13:05:19 -0400

    8/26

--- codewars.js
+++ codewars.js
@@ -7,3 +7,34 @@ function squareSum(arr) {
   }
   
   console.log(squareSum([1, 2, 2]));
+
+  // Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+function removeDuplicates(str) {
+    return str.split("").filter(word => str.indexOf() === word.lastIndexOf()).join("");
+  }
+  
+  console.log(removeDuplicates("alpha beta gamma delta alpha beta gamma delta"))
+  
+  
+//Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet
+
+function domainName(url){
+    url = url.replace("https://", '');
+    url = url.replace("http://", '');
+    url = url.replace("www.", '');
+    return url.split('.')[0];
+  };
+  
+  console.log(domainName("http://github.com/carbonfive/raygun"));
+  console.log(domainName("http://www.zombie-bites.com"));
+  console.log(domainName("https://www.cnet.com"));
+  
