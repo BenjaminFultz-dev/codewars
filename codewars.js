18387fb7e5e6302cf081e906baa7ecf66364632e
Benjamin Fultz <xsymbiotex@gmail.com>
2021-08-27 13:06:16 -0400

    8/27

--- codewars.js
+++ codewars.js
@@ -37,4 +37,30 @@ function domainName(url){
   console.log(domainName("http://github.com/carbonfive/raygun"));
   console.log(domainName("http://www.zombie-bites.com"));
   console.log(domainName("https://www.cnet.com"));
+
+  // Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+function stringTransformer(str) {
+    let newString = "";
+      for (let i = 0; i<str.length; i++){
+          if(str[i] === str[i].toLowerCase()){
+              newString += str[i].toUpperCase();
+          }else {
+              newString += str[i].toLowerCase();
+          }
+      }
+      return newString.split(" ").reverse().join(" ")
+  }
+  
+  console.log(stringTransformer("iNPUT eXAMPLE"));
+  
   
