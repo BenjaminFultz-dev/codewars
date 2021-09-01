dd1e26ca655805abd27e817cc99fb1247edfd032
Benjamin Fultz <xsymbiotex@gmail.com>
2021-09-01 13:09:34 -0400

    9/1

--- codewars.js
+++ codewars.js
@@ -146,3 +146,17 @@ console.log(solve([52,77,72,44,74,76,40]));
 console.log(solve([1,6,9,4,3,7,8,2]));
 console.log(solve([78,79,52,87,16,74,31,63,80]));
 
+//Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+// Test.assertEquals(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars');
+// Test.assertEquals(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']), 'btswmdsbd kkw'); 
+// Test.assertEquals(switcher(['4', '24']), 'wc'); 
+
+const alpha = ' zyxwvutsrqponmlkjihgfedcba!? ';
+const switcher = x => x.map(item => alpha[item]).join('');
+
+console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8']));
+console.log(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']));
+console.log(switcher(['4', '24']));
