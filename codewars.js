6736169b33b3aa89382c44c6fa4f5b28bc0f0190
Benjamin Fultz <xsymbiotex@gmail.com>
2021-09-06 13:12:51 -0400

    9/6

--- codewars.js
+++ codewars.js
@@ -248,4 +248,18 @@ function sortArray(arr) {
 console.log(sortArray([5, 3, 2, 8, 1, 4]));
 console.log(sortArray([5, 3, 1, 8, 0]));
 
+//The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+// For example:
+
+// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+
+function twoOldestAges(arr) {
+    return arr.sort((a, b) => a - b).slice(-2);
+  }
+  
+  console.log(twoOldestAges([1, 2, 10, 8]));
+  
   
