e9f561177f1615ffa0ed559c0ce8856aca76c594
Benjamin Fultz <xsymbiotex@gmail.com>
2021-09-07 13:13:35 -0400

    9/7

--- codewars.js
+++ codewars.js
@@ -261,5 +261,17 @@ function twoOldestAges(arr) {
   }
   
   console.log(twoOldestAges([1, 2, 10, 8]));
+
+  // Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+function capitals(str) {
+  
+}
+
+console.log(capitals('CodEWaRs'));
+
   
   
