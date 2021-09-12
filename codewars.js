01258aafb6dcaef920bf7482719eafaf9fd88b57
Benjamin Fultz <xsymbiotex@gmail.com>
2021-09-12 13:16:35 -0400

    9/12

--- codewars.js
+++ codewars.js
@@ -324,3 +324,19 @@ function trimString(string) {
   console.log(trimString("eloquent"));
   console.log(trimString("country"));
   
+  //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// sumMix([9, 3, '7', '3']), 22);
+// sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
+// sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
+
+function sumMix(arr) {
+    return arr.map(number => Number(number)).reduce((a, c) => a + c);
+  };
+  
+  console.log(sumMix([9, 3, '7', '3']));
+  console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]));
+  console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])); 
+  
