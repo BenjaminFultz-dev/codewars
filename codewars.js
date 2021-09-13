32a3bc6eae0169000df7906cd1ce4fcab90666a2
Benjamin Fultz <xsymbiotex@gmail.com>
2021-09-13 13:17:09 -0400

    9/13

--- codewars.js
+++ codewars.js
@@ -340,3 +340,22 @@ function sumMix(arr) {
   console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]));
   console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])); 
   
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+// getAverage([2,2,2,2]),2)
+// getAverage([1,2,3,4,5,]),3);
+// getAverage([1,1,1,1,1,1,1,2]),1)
+
+function getAverage(arr) {
+    return Math.floor(arr.reduce((a, c) => a + c) / arr.length);
+  };
+  
+  console.log(getAverage([2,2,2,2]));
+  console.log(getAverage([1,2,3,4,5,]));
+  console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]));
+  
+  
