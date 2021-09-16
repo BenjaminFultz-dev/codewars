e25c0e4a10c48e5d8b562186fc04dd63204bee01
Benjamin Fultz <xsymbiotex@gmail.com>
2021-09-16 13:19:15 -0400

    9/16

--- codewars.js
+++ codewars.js
@@ -394,3 +394,24 @@ function getThisManyEvens(arr, num) {
   console.log(getThisManyEvens([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2));
   console.log(getThisManyEvens([6, -25, 3, 7, 5, 5, 7, -3, 23], 1));
   
+  //Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+// solve([[1,2],[4],[5,6]]),4)
+// solve([[1,2],[4,4],[5,6,6]]),4)
+// solve([[1,2],[3,4],[5,6]]),8)
+// solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
+
+function countUnique(arr) {
+    const uniqueCount = arr.map(array => [...new Set(array)].length);
+    return uniqueCount.reduce((a, c) => a * c);
+  };
+  
+  console.log(countUnique([[1,2],[4],[5,6]]));
+  console.log(countUnique([[1,2],[4,4],[5,6,6]]));
+  console.log(countUnique([[1,2],[3,4],[5,6]]));
+  console.log(countUnique([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]));
+  
