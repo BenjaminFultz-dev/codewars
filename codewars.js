a0a4016293330b46d1898d08a5681156095bf481
Benjamin Fultz <xsymbiotex@gmail.com>
2021-10-11 15:03:18 -0400

    10/11

--- codewars.js
+++ codewars.js
@@ -720,5 +720,12 @@ function countSheeps(arr) {
 const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slice(-number);
 
 
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
+
+
 
 
