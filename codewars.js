6ade6b9a3dc4325cd1e304fe56a992ea38c1e5fb
Benjamin Fultz <xsymbiotex@gmail.com>
2021-10-26 15:14:21 -0400

    10/26

--- codewars.js
+++ codewars.js
@@ -872,3 +872,8 @@ function pigIt(str) {
     return word.slice(1) + firstLetter + 'ay';
   }).join(' ');
 }
+
+
+// Now you have to write a function that takes an argument and returns the square of it.
+const square = (n) => n * n;
+
