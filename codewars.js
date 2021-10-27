61e5a642ffbb8236e8ffb986a946c7bda82e8158
Benjamin Fultz <xsymbiotex@gmail.com>
2021-10-27 15:15:07 -0400

    10/27

--- codewars.js
+++ codewars.js
@@ -877,3 +877,11 @@ function pigIt(str) {
 // Now you have to write a function that takes an argument and returns the square of it.
 const square = (n) => n * n;
 
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+function getAverage(marks){
+  return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+}
+
+
