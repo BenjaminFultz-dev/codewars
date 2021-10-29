d3dfdac303dacc1037bcafd8358b2b47388ef68b
Benjamin Fultz <xsymbiotex@gmail.com>
2021-10-29 15:16:33 -0400

    10/29

--- codewars.js
+++ codewars.js
@@ -893,5 +893,11 @@ function sumCubes(n) {
   return (n * (n + 1) / 2) ** 2;
 }
 
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+function simpleMultiplication(n){
+  return n % 2 == 0 ? n * 8 : n * 9
+}
+
+
 
 
