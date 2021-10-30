837de567117355d42dcd8733d1405fe321cfe898
Benjamin Fultz <xsymbiotex@gmail.com>
2021-10-30 15:17:21 -0400

    10/30

--- codewars.js
+++ codewars.js
@@ -898,6 +898,22 @@ function simpleMultiplication(n){
   return n % 2 == 0 ? n * 8 : n * 9
 }
 
+// Task
+// Given an array/list [] of integers , Find the Nth smallest element in this array of integers
+// ________________________________________
+// Notes
+// •	Array/list size is at least 3 .
+// •	Array/list's numbers could be a mixture of positives , negatives and zeros .
+// •	Repetition in array/list's numbers could occur , so don't Remove Duplications .
+// ________________________________________
+// Input >> Output Examples
+// nthSmallest({3,1,2} ,2) ==> return (2) 
+// Explanation:
+// Since the passed number is 2 , Then * the second smallest element in this array/list is 2*
+function nthSmallest(arr, pos){
+  return arr.sort((a,b)=>a-b)[pos-1]
+}
+
 
 
 
