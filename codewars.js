bcf74ddead846bb897bdd3414a180da119a74aad
Benjamin Fultz <xsymbiotex@gmail.com>
2021-11-01 15:18:45 -0400

    11/1

--- codewars.js
+++ codewars.js
@@ -924,6 +924,13 @@ function digital_root(n) {
   return (n - 1) % 9 + 1;
 }
 
+// Create a method take that accepts a list/array and a number n, and returns a list/array array of the first n elements from the list/array.
+// If you need help, here's a reference:
+function take(arr, n) {
+  return arr.slice(0, n);
+}
+
+
 
 
 
