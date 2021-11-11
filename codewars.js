73ff3d99aeab0eab3d165a6e12682da4dfb5193e
Benjamin Fultz <xsymbiotex@gmail.com>
2021-11-11 11:35:24 -0500

    11/11

--- codewars.js
+++ codewars.js
@@ -985,6 +985,21 @@ function high(s){
   return s.split(' ')[as.indexOf(Math.max(...as))];
 }
 
+// Description:
+// Imagine you start on the 5th floor of a building, then travel down to the 2nd floor, then back up to the 8th floor. You have travelled a total of 3 + 6 = 9 floors of distance.
+// Given an array representing a series of floors you must reach by elevator, return an integer representing the total distance travelled for visiting each floor in the array in order.
+// // simple examples
+// elevatorDistance([5,2,8]) = 9
+// elevatorDistance([1,2,3]) = 2
+// elevatorDistance([7,1,7,1]) = 18
+// // if two consecutive floors are the same,
+// //distance travelled between them is 0
+// elevatorDistance([3,3]) = 0
+// Array will always contain at least 2 floors. Random tests will contain 2-20 elements in array, and floor values between 0 and 30.
+
+function elevatorDistance(array) {
+  return array.map((v, i, arr) => arr[i] - arr[i - 1]).filter(v => !isNaN(v)).reduce((a, b) => a + Math.abs(b), 0);
+}
 
 // Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
 // a is considered "close to" b if margin is greater than or equal to the distance between a and b.
