52d4dde721b211f231e7c23b7661fa04f8eeb236
Benjamin Fultz <xsymbiotex@gmail.com>
2021-11-13 11:55:53 -0500

    11/13

--- codewars.js
+++ codewars.js
@@ -1065,3 +1065,18 @@ function missingVowel(str){
     }
   }
 }
+
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
+
+let replaceDots = function(str) {
+  return str.replace(/\./g,'-');
+}
+
