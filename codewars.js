520845e836885dd4a8b432a897e07ca5d9645308
Benjamin Fultz <xsymbiotex@gmail.com>
2021-11-16 12:01:25 -0500

    11/16

--- codewars.js
+++ codewars.js
@@ -1106,3 +1106,20 @@ function count (string) {
   return count;
 }
 
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+function toCamelCase(str){ 
+  let arr = str.split('');
+  for(i = 0; i < arr.length; i++){
+    let letter = arr[i];
+    if(letter == '_' || letter == '-') {
+      arr[i + 1] = arr[i + 1].toUpperCase();
+      arr[i] = '';
+    } 
+  }
+  return arr.join('');
+}
