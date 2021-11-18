35a937652f83552f2df3209d078ad1c633b1cc77
Benjamin Fultz <xsymbiotex@gmail.com>
2021-11-18 12:19:36 -0500

    11/18

--- codewars.js
+++ codewars.js
@@ -1148,3 +1148,16 @@ function tribonacci(signature,n){
   }
   return signature.slice(0, n); //return trib - length of n
 }
+
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
+
+function domainName(url){
+  return  url.replace('http://', '')
+             .replace('https://', '')
+             .replace('www.', '')
+             .split('.')[0];
+}
