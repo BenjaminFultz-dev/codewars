069f267cf9574ac289f5bfe60cb0b604337e6f66
Benjamin Fultz <xsymbiotex@gmail.com>
2021-11-20 15:47:21 -0500

    11/20

--- codewars.js
+++ codewars.js
@@ -1182,3 +1182,20 @@ const corrections = {
 const correct = string => (
   string.replace(/[501]/g, character => corrections[character])
 );
+
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+  let petals = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'];
+  return petals[(nbPetals-1) % 6];
+}
