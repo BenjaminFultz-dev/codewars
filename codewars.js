cd59f6fbf20798f153802209ea7554d2dccb2883
Benjamin Fultz <xsymbiotex@gmail.com>
2021-11-30 14:58:28 -0500

    11/30

--- codewars.js
+++ codewars.js
@@ -1344,3 +1344,21 @@ function hoopCount (n) {
 function find_difference(a, b) {
   return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
 }
+
+// Write a function named numbers.
+
+// function should return True if all parameters are of the Number type.
+
+// The function should accept any number of parameters.
+
+// Example usage:
+
+// numbers(1, 4, 3, 2, 5); // true
+// numbers(1, "a", 3); // false
+// numbers(1, 3, NaN); // true
+
+function numbers() {
+  return [].every.call(arguments, function(value) {
+    return typeof value === "number";
+  });
+}
