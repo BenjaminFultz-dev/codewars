d0fb2b54776aa21fae7d71290078580a18d99fc9
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-06 16:15:07 -0500

    12/6

--- codewars.js
+++ codewars.js
@@ -1476,3 +1476,30 @@ const sc = screws => {
 }
 
 
+// John is a worker, his job is to remove screws from a machine. There are 2 types of screws: slotted (-) and cross (+). John has two screwdrivers, one for each type of screw.
+
+// The input will be a (non-empty) string of screws, e.g. : "---+++"
+
+// When John begins to work, he stands at the first screw, with the correct screwdriver in his hand, and another in his tool kit. He works from left to right, removing every screw. When necessary, he switches between the screwdriver in his hand and the one in his tool kit.
+
+// Each action takes a set amount of time:
+
+// remove one screw : 1 second
+// move to the next screw: 1 second
+// switch screwdrivers: 5 seconds
+
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+
+const FilterString = function(value) {
+  return parseInt(value.replace(/[^\d]/g, ""))
+}
+
+
