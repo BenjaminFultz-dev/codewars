1088b3326b9609dc5473e83aa6756e37692e9774
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-08 11:19:56 -0500

    12/8

--- codewars.js
+++ codewars.js
@@ -1515,3 +1515,9 @@ const FilterString = function(value) {
 function disemvowel(str) {
   return str.replace(/[aeiou]/gi, '');
 }
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
