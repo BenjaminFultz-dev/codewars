fe8bc99fb69456c8626929a223242b130b2dbdac
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-14 16:25:47 -0500

    12/14

--- codewars.js
+++ codewars.js
@@ -1657,3 +1657,16 @@ function solve(s){
     }
     return lowerC >= upperC ? s.toLowerCase() : s.toUpperCase()
 }
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples:
+
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+const number = array => array.map((n, i) => `${i + 1}: ${n}`);
