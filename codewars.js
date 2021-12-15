5ed6b22e58fdfcd5d8e0a0dfef664cdd21b9c4b2
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-15 10:49:40 -0500

    12/15

--- codewars.js
+++ codewars.js
@@ -1670,3 +1670,18 @@ function solve(s){
 // number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
 
 const number = array => array.map((n, i) => `${i + 1}: ${n}`);
+
+// Triple Trouble
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Note: You can expect all of the inputs to be the same length.
+
+function tripleTrouble(one, two, three) {
+  var result = "";
+  for (let i = 0; i < one.length; i++) {
+    result += one.charAt(i) + two.charAt(i) + three.charAt(i);
+  }
+  return result;
+}
