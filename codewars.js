dcf89930d87f61dba9866abafba08ee341895e97
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-18 15:12:23 -0500

    12/18

--- codewars.js
+++ codewars.js
@@ -1718,3 +1718,15 @@ function stringTransformer(str) {
 function capitalizeWord(word) {
   return word[0].toUpperCase() + word.slice(1);
 }
+
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+// For example:
+
+// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+
+function twoOldestAges(ages){
+  return ages.sort(function(a,b){return a-b;}).slice(-2);
+}
