4228999fe7c4172b9351bc52ca61b4cbe220edce
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-19 14:49:57 -0500

    12/19

--- codewars.js
+++ codewars.js
@@ -1730,3 +1730,11 @@ function capitalizeWord(word) {
 function twoOldestAges(ages){
   return ages.sort(function(a,b){return a-b;}).slice(-2);
 }
+
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+const removeConsecutiveDuplicates = s => s.split(" ").filter((x,i,arr) => x!=arr[i-1]).join(" ");
