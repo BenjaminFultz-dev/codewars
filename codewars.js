8f0a1689d360a1995d73a0bad458e6cfa4b50f0c
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-21 13:15:09 -0500

    12/21

--- codewars.js
+++ codewars.js
@@ -1750,3 +1750,11 @@ const removeConsecutiveDuplicates = s => s.split(" ").filter((x,i,arr) => x!=arr
 function repeats(arr){
   return arr.filter(v => arr.indexOf(v) === arr.lastIndexOf(v)).reduce((a,b) => a + b, 0);
 };
+
+// In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+const solve = arr =>  arr.map((el) => [...new Set(el)].length).reduce((acc, c) => acc * c)
