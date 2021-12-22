f0b23ebb1c3c6e3fdb3a1fe98ad7cbca60bfebb5
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-22 13:15:47 -0500

    12/22

--- codewars.js
+++ codewars.js
@@ -1758,3 +1758,22 @@ function repeats(arr){
 // Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
 
 const solve = arr =>  arr.map((el) => [...new Set(el)].length).reduce((acc, c) => acc * c)
+
+
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme(a) {
+  return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+}
