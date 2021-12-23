67ef1f430baf10ebe62c93c7e61d029d1683948b
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-23 13:16:32 -0500

    12/23

--- codewars.js
+++ codewars.js
@@ -1777,3 +1777,24 @@ const solve = arr =>  arr.map((el) => [...new Set(el)].length).reduce((acc, c) =
 function gimme(a) {
   return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
 }
+
+// In this Kata, you will be given an array of unique elements, and your task is to rearrange the values so that the first max value is followed by the first minimum, followed by second max value then second min value, etc.
+
+// For example:
+
+// solve([15,11,10,7,12]) = [15,7,12,10,11]
+// The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
+
+// More examples in the test cases.
+
+// Good luck!
+
+function solve(arr){
+  var minMax = [];
+  arr.sort((a,b)=>a-b);
+  for(var i = 0, j = arr.length-1; i <= j; i++,j--){
+    if(i != j) minMax.push(arr[j]), minMax.push(arr[i]);
+    else minMax.push(arr[i]);
+  }
+  return minMax;
+}
