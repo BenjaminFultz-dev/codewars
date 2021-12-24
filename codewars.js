9b603b38502ebc4048bb2959849c2f5c776cb2de
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-24 13:17:13 -0500

    12/24

--- codewars.js
+++ codewars.js
@@ -1798,3 +1798,21 @@ function solve(arr){
   }
   return minMax;
 }
+
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+// Good luck!
+
+function dup(s) {
+  return s.map(w => {
+    return w.split('').filter((c, i, arr) => {
+      return c !== arr[i - 1];
+    }).join('');
+  });
+};
