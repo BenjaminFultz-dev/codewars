ba348bfef926b00ddabb70db307a7a3e9e536f5f
Benjamin Fultz <xsymbiotex@gmail.com>
2021-12-31 14:32:43 -0500

    12/31

--- codewars.js
+++ codewars.js
@@ -1868,3 +1868,15 @@ function findDeletedNumber(arr, mixArr) {
 
 const alpha = ' zyxwvutsrqponmlkjihgfedcba!? '
 const switcher = x => x.map(item => alpha[item]).join('')
+
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count (string) {  
+  var count = {};
+  string.split('').forEach(function(s) {
+     count[s] ? count[s]++ : count[s] = 1;
+  });
+  return count;
+}
