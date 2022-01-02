5daca2e17294d0d92528146b14ea4bd0723a5e0d
Benjamin Fultz <xsymbiotex@gmail.com>
2022-01-02 14:34:01 -0500

    1/2

--- codewars.js
+++ codewars.js
@@ -1892,3 +1892,22 @@ function sortArray(array) {
   const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
   return array.map((x) => x % 2 ? odd.shift() : x);
 }
+
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+    var z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}
