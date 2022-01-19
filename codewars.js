7cb780d92a7c848fd37d2bf670e5bc0805d77700
Benjamin Fultz <xsymbiotex@gmail.com>
2022-01-19 14:39:49 -0500

    01/19

--- codewars.js
+++ codewars.js
@@ -2002,3 +2002,9 @@ function numberToString(num) {
 
 const nthEven=n=>n*2-2;
 
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(n) {
+    return n * (n % 2 ? 9 : 8);
+}
+
