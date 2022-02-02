c8353497e1c32c5fbd909cbbc2bee49d58276ea4
Benjamin Fultz <xsymbiotex@gmail.com>
2022-02-02 14:50:10 -0500

    02/02

--- codewars.js
+++ codewars.js
@@ -2203,3 +2203,15 @@ const isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
 function numberToString(num) {
   return num.toString();
 }
+
+// Return the Nth Even Number
+
+// Example(Input --> Output)
+
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+// The input will not be 0.
+
+const nthEven=n=>n*2-2;
